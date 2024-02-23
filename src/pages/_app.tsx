b4dc041e6ced239c4a1d0717dbb2dc56b66cf44a
Kenny Tran <82717216+nytrek@users.noreply.github.com>
2024-02-23 23:03:03 +0100

    set up custom font and manual dark mode

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Public_Sans } from "next/font/google";
+
+const public_sans = Public_Sans({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${public_sans.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  );
 }
