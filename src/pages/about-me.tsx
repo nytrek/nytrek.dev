67e0f8a35a7b3d91b0fe47dd48bbea29ede07ca4
Kenny Tran <82717216+nytrek@users.noreply.github.com>
2024-02-24 21:25:39 +0100

    fix linter

--- src/pages/about-me.tsx
+++ src/pages/about-me.tsx
@@ -16,18 +16,18 @@ export default function Page() {
         </h1>
         <div className="grid gap-y-6 text-lg leading-8 text-zinc-600 dark:text-zinc-300">
           <p>
-            I'm Kenny Tran, a seasoned developer with a passion for crafting
-            innovative and scalable solutions. With over three years of
+            I&apos;m Kenny Tran, a seasoned developer with a passion for
+            crafting innovative and scalable solutions. With over three years of
             experience in the dynamic realm of full-stack development, I thrive
             on the excitement of building something new and tackling fresh
             challenges head-on.
           </p>
           <p>
-            In my professional journey, I've collaborated closely with diverse
-            teams, from product owners to graphic designers, translating
+            In my professional journey, I&apos;ve collaborated closely with
+            diverse teams, from product owners to graphic designers, translating
             intricate product strategies into tangible, sustainable solutions.
-            Whether it's developing modern websites for international clients or
-            architecting robust SaaS platforms like{" "}
+            Whether it&apos;s developing modern websites for international
+            clients or architecting robust SaaS platforms like{" "}
             <Link
               className="underline hover:no-underline"
               href="https://www.bostadsval.se/"
@@ -36,25 +36,25 @@ export default function Page() {
             >
               Bostadsval
             </Link>{" "}
-            for real estate advertisements, I've consistently strived for
+            for real estate advertisements, I&apos;ve consistently strived for
             excellence in every project I undertake.
           </p>
           <p>
-            Outside of the tech realm, I'm fluent in both English and Swedish,
-            and I cherish moments spent connecting with others on a personal
-            level. Whether it's mentoring junior colleagues, coordinating
-            integrations between programs, or simply engaging in meaningful
-            conversations, I believe in fostering strong relationships built on
-            trust, respect, and collaboration.
+            Outside of the tech realm, I&apos;m fluent in both English and
+            Swedish, and I cherish moments spent connecting with others on a
+            personal level. Whether it&apos;s mentoring junior colleagues,
+            coordinating integrations between programs, or simply engaging in
+            meaningful conversations, I believe in fostering strong
+            relationships built on trust, respect, and collaboration.
           </p>
           <p>
-            I'm excited about the opportunity to connect with like-minded
+            I&apos;m excited about the opportunity to connect with like-minded
             individuals who share my enthusiasm for innovation and growth,
-            whether it's in the professional arena or beyond. If you're
-            interested in exploring potential collaborations, discussing tech
-            trends, or simply grabbing a coffee and exchanging ideas, feel free
-            to reach out! I'm always up for a stimulating conversation or an
-            exciting coding challenge.
+            whether it&apos;s in the professional arena or beyond. If
+            you&apos;re interested in exploring potential collaborations,
+            discussing tech trends, or simply grabbing a coffee and exchanging
+            ideas, feel free to reach out! I&apos;m always up for a stimulating
+            conversation or an exciting coding challenge.
           </p>
         </div>
       </main>
