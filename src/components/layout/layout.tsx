28c8725a94425b1b675b18e65b9aa3ee93ee3809
Kenny Tran <82717216+nytrek@users.noreply.github.com>
2024-02-24 23:26:10 +0100

    implement swipe handlers

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,9 +1,11 @@
 import { directionContext } from "@/context/direction";
+import useSwipe from "@/hooks/useSwipe";
 import { AnimatePresence, motion } from "framer-motion";
 import { useRouter } from "next/router";
 import { useContext } from "react";
 import { Footer } from "../footer";
 import { Header } from "../header";
+import { tabs } from "../header/data";
 import { variants } from "./variants";
 
 /**
@@ -12,10 +14,26 @@ import { variants } from "./variants";
  * @see https://github.com/pacocoursey/next-themes?tab=readme-ov-file#usetheme
  */
 export const Layout: React.FC<React.PropsWithChildren> = ({ children }) => {
-  const { route } = useRouter();
-  const { direction } = useContext(directionContext);
+  const { push, route } = useRouter();
+  const { direction, setDirection } = useContext(directionContext);
+  const onSwipe = (swipe: "left" | "right") => {
+    if (swipe === "left") {
+      push(tabs[tabs.findIndex((item) => item.href === route) + 1].href);
+    } else {
+      push(tabs[tabs.findIndex((item) => item.href === route) - 1].href);
+    }
+    setDirection(swipe);
+  };
+  const swipeHandlers = useSwipe({
+    onSwipedLeft: () =>
+      tabs.findIndex((item) => item.href === route) === tabs.length - 1
+        ? null
+        : onSwipe("left"),
+    onSwipedRight: () =>
+      !tabs.findIndex((item) => item.href === route) ? null : onSwipe("right"),
+  });
   return (
-    <div className="py-24 sm:py-32">
+    <div className="py-24 sm:py-32" {...swipeHandlers}>
       <div className="mx-auto grid max-w-4xl gap-y-6 px-6 lg:px-8">
         <Header />
         <AnimatePresence
